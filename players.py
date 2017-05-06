7da9e494b93844a66ea2b69a2159aeec4bd19195
Jeffrey Buehler <jeffreydeanbuehler@gmail.com>
2017-05-05 22:39:16 -0600

    initialize AIPlayer with vars

--- players.py
+++ players.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 class RandomPlayer:
 	def __init__(self, seed=None):
 		self.playernum = 0
@@ -16,9 +15,22 @@ class RandomPlayer:
 
 
 class AIPlayer:
-	def __init__(self):
+	def __init__(self, rho=0.2, epsilonDecayRate = 0.99, epsilon=1.0):
 		self.playernum = 0
+		self.rho = rho
+		self.epsilonDecayRate = epsilonDecayRate
+		self.epsilon = epsilon
+		self.Q = {}
 
 	def pickmove(self, game):
 		moves = game.validmoves()
-		return None
+		nextMove = self.__epsilonGreedy(game)
+		return nextMove
+
+	def __epsilonGreedy(self, game):
+		validMoves = game.validmoves()
+		if np.random.uniform() < self.epsilon:
+			return np.random.choice(validMoves)
+		else:
+			Qs = np.array([self.Q.get((tuple(validMoves), m), 0) for m in validMoves])
+			return validMoves[np.argmax(Qs)]
